a3ac8aabc275a6b39ca4ce1de0912baf1d5dedd1
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-07 21:00:46 -0300

    request url

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import http from "node:http";
 
 const server = http.createServer((request, response) => {
-  const { method } = request;
-  
-  return response.writeHead(404).end("Método usado: " + method)
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/products") {
+    return response.writeHead(200).end("Listagem de produtos  realizada!");
+  }
+
+  if (method === "POST" && url === "/products") {
+    return response.writeHead(201).end("Produto cadastrado!");
+  }
+
+  return response.writeHead(404).end("Rota não encontrada!");
 });
 
 server.listen(3333);
