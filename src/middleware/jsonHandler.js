6acc550dbe460953b8946b282563e345e80cfcbb
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-08 15:57:35 -0300

    making middleware

---
+++ src/middleware/jsonHandler.js
@@ -0,0 +1,19 @@
+export async function jsonBodyHandler(request, response) {
+  //adicionar cada chunk(pedaços) de dados em um array
+  const buffers = [];
+
+  //recuperando os chunk de dados da requisição
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    //concatenar os chunk e converter em string. Converte a string em JSON.
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (error) {
+    request.body = null;
+  }
+
+  //define o header da resposta como JSON
+  response.setHeader("Content-type", "application/json");
+}
--- src/server.js
+++ src/server.js
@@ -1,24 +1,18 @@
-import { log } from "node:console";
 import http from "node:http";
+import { jsonBodyHandler } from "./middleware/jsonHandler.js";
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
+
+  //adicionando o middleware - monta o corpo da requisição
+  await jsonBodyHandler(request, response);
 
   if (method === "GET" && url === "/products") {
     return response.writeHead(200).end("Listagem de produtos  realizada!");
   }
 
   if (method === "POST" && url === "/products") {
-    const buffers = [];
-
-    for await (const chunk of request) {
-      buffers.push(chunk);
-    }
-
-    console.log(Buffer.concat(buffers).toString());
-    
-
-    return response.writeHead(201).end("Produto cadastrado!");
+    return response.writeHead(201).end(JSON.stringify(request.body));
   }
 
   return response.writeHead(404).end("Rota não encontrada!");
